ba50497f56211ba75a5de9992a65aa53ca2500e8
Dezhao-Jia <49218570+Dezhao-Jia@users.noreply.github.com>
2024-06-09 09:47:46 +0800

    Create Corrdinate.py

---
+++ attention_model/Corrdinate.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+
+from einops import rearrange
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class CoordAtt(nn.Module):
+    def __init__(self, in_channels, out_channels, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, in_channels // reduction)
+
+        self.conv1 = nn.Conv2d(in_channels, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+
+        self.conv_h = nn.Conv2d(mip, out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, out_channels, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        if x.shape[2] == 1:
+            x = rearrange(x, 'n c h w -> n h c w')
+
+        identity = x
+
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+        out = rearrange(out, 'n h c w -> n c h w')
+
+        return out
+
+
+if __name__ == '__main__':
+    x = torch.rand(1, 160, 1, 69)
+    mode = CoordAtt(in_channels=1, out_channels=1)
+
+    o = mode(x)
+    print(o.shape)
